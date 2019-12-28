3d35f8f878efa7210a9dd978c8032ece46e8f890
Ali Uz <ali.uz@gmx.at>
2019-12-28 22:38:17 +0100

    Flightbooking initial commit

--- src/FsFhtw/Domain.fs
+++ src/FsFhtw/Domain.fs
@@ -1,5 +1,64 @@
 module Domain
 
+// Optional:
+// OverbookingRate
+// Compensation
+
+type Airport = 
+    | IATA of string
+    | ICAO of string
+    | FAA of string
+
+type Luggage = bool
+
+type Person =
+    { FirstName: string
+      LastName: string
+      Birthday: string }
+
+type Passenger = 
+    { Person: Person
+      FrequentFlyerProgramId: string option }
+
+type FlightDesignator = 
+    { Airline: string 
+      Number: int }
+
+type Flight =
+    { Designator: FlightDesignator 
+      DepartureTime: System.DateTime
+      Duration: System.TimeSpan
+      DepartureAirport: Airport
+      ArrivalAirport: Airport }
+
+type PaymentState =
+    | Error of string
+    | Success
+
+type PaymentMethod =
+    | Cash
+    | PayPal of PaymentState 
+    | CreditCard of PaymentState 
+
+type Price =
+    { Amount: decimal
+      Currency: string }
+
+type Payment =
+    { Method: PaymentMethod
+      Price: Price }
+
+type BoardingInfo = 
+    { Gate: string option
+      Seat: string option }
+
+type Booking = 
+    { Passenger: Passenger
+      PaymentInfo: Payment
+      Flight: Flight
+      BoardingInfo: BoardingInfo option
+      Luggage: Luggage }
+
 type State = int
 
 type Message =
