895b12eae7ac25bb134e4cdeb67db87973045e34
Ali Uz <ali.uz@gmx.at>
2020-01-19 17:12:27 +0100

    Add test project

---
+++ tests/FsFhtw.Tests/Library.fs
@@ -0,0 +1,11 @@
+﻿module FsFhtw.Tests
+
+open Xunit
+open FsUnit
+
+[<Fact>]
+let ``That the setup is correct`` () =
+    let actual = 1
+    let expected = 1
+    
+    actual |> should equal expected
